d8bcded02fc2ed9e41549b9fc3ea21ab2079433d
aeon <jerry73204@gmail.com>
2023-02-08 12:55:09 +0800

    Add calibration data types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 pub mod annotation;
+pub mod calibration;
 pub mod error;
 pub mod iter;
 pub mod vec;
 pub mod write;
 
 pub use annotation::*;
+pub use calibration::*;
 pub use error::Error;
+
+pub mod prelude {
+    #[cfg(feature = "nalgebra")]
+    pub use crate::calibration::NalgebraCalib as _;
+}
