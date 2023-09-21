28f8f6ad196f52e26671e115e274ce21ceafef25
aeon <jerry73204@gmail.com>
2023-09-21 10:21:14 +0800

    Add odometry calib test data.

--- tests/calib.rs
+++ tests/calib.rs
@@ -1,6 +1,7 @@
-use kitti_dataset::CameraCalibration;
+use kitti_dataset::{CameraCalibration, OdometryCalibration};
 
 #[test]
 fn parse_calib() {
-    let _calib = CameraCalibration::from_path("tests/calib.txt").unwrap();
+    let _ = CameraCalibration::from_path("tests/camera_calib.txt").unwrap();
+    let _ = OdometryCalibration::from_path("tests/odometry_calib.txt").unwrap();
 }
